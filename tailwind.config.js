a4e0511a30b281c8dd70574ff228056254d1c22a
Kasey McFadden <kaseysmcfadden@gmail.com>
2025-06-04 15:10:59 -0400

    Add cmu-serif font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        cmu: ['"CMU Serif"', 'serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
