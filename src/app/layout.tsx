b028daae5f55237b217bdd1b182f6bb6229bd057
Kasey McFadden <kaseysmcfadden@gmail.com>
2026-01-18 21:42:51 -0500

    feat: preload custom fonts in layout for perf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
+import type { Metadata } from "next";
 
 import "./globals.css";
 
@@ -16,6 +16,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="preload"
+          href="/cv/fonts/cmu-serif/cmunrm.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/cv/fonts/cmu-serif/cmunbx.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>{children}</body>
       <Analytics />
     </html>
